fd135dfbde53ee9a8e0d89bb8b73bfeb4a75444a
Ranajoy Das <rdas19999@gmail.com>
2026-09-04 13:11:56 +0530

    fix: browser fix for linux env

--- src/main/java/com/automation/driver/DriverFactory.java
+++ src/main/java/com/automation/driver/DriverFactory.java
@@ -2,6 +2,7 @@ package com.automation.driver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -12,6 +13,12 @@ public class DriverFactory {
 
     @BeforeMethod
     public WebDriver setup(){
+
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
 
         driver=new ChromeDriver();
         driver.manage().window().maximize();
